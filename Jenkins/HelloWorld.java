a6a7cf254bd55f736a86bc7c3c6e59e8011afadd
piyal <piyal.adamas2@gmail.com>
2026-04-25 17:36:47 +0530

    testing auto integration

--- Jenkins/HelloWorld.java
+++ Jenkins/HelloWorld.java
@@ -3,5 +3,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome Piyal,Good to learn DevOps Happy Jenkins learning Journey");
+        System.out.println("Automatic Integration Successful");
     }
 }
